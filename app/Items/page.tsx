4e28844cccd7b1e8ac2cede43ffb1347d59984eb
VolvosInSpace <g36evan@gmail.com>
2024-04-14 13:43:49 -0400

    addded an actual small items page, will add more items in the future when I feel like it

--- app/Items/page.tsx
+++ app/Items/page.tsx
@@ -1,82 +1,65 @@
 import Link from 'next/link';
-import '@/app/ui/cart.css';
+import styles from '@/app/ui/style.css';
 
-export default function Cart() {
+export default function Page() {
   return (
     <div>
-      <header>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/Items">Items</Link>
-        </nav>
-      </header>
-      <main>
-        <h1>Here are all the current items we have in stock</h1>
-        <table className="item-table">
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Price</th>
-              <th>Origin Store</th>
-              <th>Freshness Rating</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Apple Pie</td>
-              <td>$10.00</td>
-              <td>Bakery</td>
-              <td>10/10</td>
-            </tr>
-            <tr>
-              <td>Loaf of Bread</td>
-              <td>$5.00</td>
-              <td>Bakery</td>
-              <td>9/10</td>
-            </tr>
-            <tr>
-              <td>Cheese Pizza</td>
-              <td>$10.00</td>
-              <td>Rocco's Pizzera</td>
-              <td>Hot n' Ready</td>
-            </tr>
-            <tr>
-              <td>Beef Wellington</td>
-              <td>$10.00</td>
-              <td>Dave's Butcher</td>
-              <td>9/10</td>
-            </tr>
-            <tr>
-              <td>Milk</td>
-              <td>$3</td>
-              <td>Green Grocer</td>
-              <td>8/10</td>
-            </tr>
-            <tr>
-              <td>Eggs</td>
-              <td>$2</td>
-              <td>Green Grocer</td>
-              <td>9/10</td>
-            </tr>
-            <tr>
-              <td>Apples</td>
-              <td>$4</td>
-              <td>Green Grocer</td>
-              <td>9/10</td>
-            </tr>
-            <tr>
-              <td>Bananas</td>
-              <td>$3</td>
-              <td>Green Grocer</td>
-              <td>Ripe</td>
-            </tr>
-          </tbody>
-        </table>
-
-      <section id="checkout">
-      <Link href="/cart">Cart</Link>
-      </section>
-      </main>
-    </div>
+        <div className={styles.shape} />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/customers/amy-burns.png" />
+        <meta name="viewport" content="width=device-width" />
+        <title>Grocery Store</title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet" />
+        <div className="banner">
+          <header>
+            <h1>Grocery Store</h1>
+            <nav>
+              <Link href="/">Home</Link>
+              <Link href="/Items">Items</Link>
+              <Link href="/cart">Cart</Link>
+            </nav>
+          </header>
+          <div className="banner-text">
+            <p>Better Food Locally</p>
+            <h1>Starts With You</h1>
+            <a href="/cart">Look At Your Cart</a>
+          </div>
+        </div>
+        <div className="main-content">
+          <div className="one-col">
+            <h1>Here is ours favorite items of the day</h1>
+            <p>Here at grocery Store TM, We love food, so thats why we bring you the best food.</p>
+          </div>
+          <div className="two-col">
+            <div className="two-col1">
+              <img src="/customers/apple-pie-transparent-png.png" />
+            </div>
+            <div className="two-col2">
+              <h1>Grandma's Apple Pie</h1>
+              <p>Apple pie is a classic dessert that has become synonymous with comfort and tradition in many cultures around the world. 
+                This delectable pastry consists of a sweet and spiced apple filling encased in a flaky pastry crust, 
+                typically served warm with a scoop of vanilla ice cream or a dollop of whipped cream.
+              </p>
+              <a href="#">Add To Cart</a>
+            </div>
+          </div>
+          <div className="two-col">
+            <div className="two-col2">
+              <h1>Garlic Bread from Heaven</h1>
+              <p>Garlic bread is a mouthwatering culinary creation that delights taste buds with its irresistible combination of flavors and textures. 
+                This beloved dish features slices of bread generously slathered with a savory mixture of garlic-infused butter, herbs, 
+                and often grated Parmesan cheese. Once baked or toasted to golden perfection, 
+                garlic bread emerges from the oven with a tantalizing aroma that promises a truly satisfying dining experience.
+              </p>
+              <a href="#">Add To Cart</a>
+            </div>
+            <div className="two-col1">
+              <img src="/customers/Garlic-Bread.png" />
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
